7362a3074b4953c7827155fff6656d23e82272d9
HyeJeong <rucy011121@gmail.com>
2024-05-21 01:43:47 +0900

    :tada: Initial commit

---
+++ HyeJeong/hipet/src/test/java/com/example/hipet/HipetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hipet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HipetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
