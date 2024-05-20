a387f48e11480b768dc6dfddd375afa4388fb127
HyeJeong <rucy011121@gmail.com>
2024-05-21 01:43:47 +0900

    :tada: Initial commit

---
+++ HyeJeong/hipet/src/main/java/com/example/hipet/HipetApplication.java
@@ -0,0 +1,13 @@
+package com.example.hipet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HipetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HipetApplication.class, args);
+	}
+
+}
