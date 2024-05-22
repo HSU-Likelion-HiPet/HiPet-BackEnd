e5afc939d43322ed805d8271cd6adddceb52b7e6
HyeJeong <rucy011121@gmail.com>
2024-05-22 15:54:58 +0900

    :recycle: CHORE : User 도메인 수정

--- HyeJeong/hipet/src/main/java/com/example/hipet/domain/User.java
+++ HyeJeong/hipet/src/main/java/com/example/hipet/domain/User.java
@@ -13,25 +13,31 @@ import util.entity.BaseEntity;
 public class User extends BaseEntity {
     @Id
     @GeneratedValue
+    @Column(name="USER_ID")
+    private String userId; //고유의 아이디 - 사용자에게 부여되는
+
     @Column(name="LOGIN_ID")
     private String loginId; //로그인 아이디
 
     @Column(name="PASSWORD")
     private String password; //비밀번호
 
-    @Column(name="PHONE_NUMBER")
-    private String phoneNumber; //전화번호
-
-    @Column(name="ADDRESS")
-    private String address; //주소 -> 범위는 미정..
+    @Column(name="USER_NAME")
+    private String userName; //사용자 이름
 
     @Column(name="NICKNAME")
     private String nickname; //닉네임
 
-    //"loginId" : "pet",
-    //    "password" : "1234",
-    //    "phoneNumber" : "01012345678",
-    //    "address" : "서울"
-    //    "nickname" : "하이펫"
+    @Column(name="ADDRESS")
+    private String address; //주소 -> 범위는 미정..
+
+    @Column(name="PHONE_NUMBER")
+    private String phoneNumber; //전화번호
+
+    @Column(name="PROFILE_INFO")
+    private String profileInfo; //사용자 소개
+
+
+
 
 }
