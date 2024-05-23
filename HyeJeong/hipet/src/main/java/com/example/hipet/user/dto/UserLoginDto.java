d02276e12b4bb80df9e9492a63d6845e9445081c
HyeJeong <rucy011121@gmail.com>
2024-05-23 23:17:51 +0900

    :recycle: CHORE: 정규패턴식 수정

--- HyeJeong/hipet/src/main/java/com/example/hipet/user/dto/UserLoginDto.java
+++ HyeJeong/hipet/src/main/java/com/example/hipet/user/dto/UserLoginDto.java
@@ -9,7 +9,6 @@ import lombok.Setter;
 @Getter @Setter
 @AllArgsConstructor
 public class UserLoginDto {
-    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "아이디 형식을 맞춰주세요")
     @NotEmpty(message = "아이디가 필요합니다.")
     private String loginId;
 
