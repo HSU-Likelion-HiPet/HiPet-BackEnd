17268f0308e862ac0f5e8112381791c785010779
= <=>
2024-05-25 21:06:49 +0900

    :fire: CHORE. 코드 개선을 위한 findByAnimalId 메서드 제거

--- src/main/java/com/hipet/domain/animal/repository/AnimalRepository.java
+++ src/main/java/com/hipet/domain/animal/repository/AnimalRepository.java
@@ -3,5 +3,8 @@ package com.hipet.domain.animal.repository;
 import com.hipet.domain.animal.entity.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
+    Optional<Animal> findByAnimalId(Long animalId);
 }
