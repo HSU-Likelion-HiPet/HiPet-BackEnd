7cf45521825f091b638eb2df2b52bad8e47559fd
= <=>
2024-05-25 21:02:03 +0900

    :art: CHORE. Entity Enum fields @Enumerated annotation 추가 및 양방향 연관계 코드 추가

--- src/main/java/com/hipet/domain/animal/entity/Animal.java
+++ src/main/java/com/hipet/domain/animal/entity/Animal.java
@@ -2,8 +2,10 @@ package com.hipet.domain.animal.entity;
 
 import com.hipet.domain.User.entity.User;
 import com.hipet.domain.animal.enums.Gender;
+import com.hipet.domain.review.entity.Review;
 import com.hipet.global.entity.BaseEntity;
 import com.hipet.global.enums.Category;
+import com.hipet.global.enums.Region;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,8 +29,13 @@ public class Animal extends BaseEntity {
 
     private String price;
 
+    @Enumerated(EnumType.STRING)
     private Category category;
 
+    @Enumerated(EnumType.STRING)
+    private Region region;
+
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
     private String information;
@@ -40,8 +47,14 @@ public class Animal extends BaseEntity {
     @JoinColumn(name = "userId")
     private User user;
 
+    // 양방향 연관관계 매핑을 할 때, 연관관계의 주인을 설정해주어야 한다.
+    // 연관관계의 주인은 1:N 의 경우 N쪽에 해주면 된다.
+    // mappedBy의 값은 반대쪽에 자신이 매핑되어있는 필드명을 써주면 된다.
     @OneToMany(mappedBy = "animal", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<HashTag> hashTag;
+
+    @OneToMany(mappedBy = "animalId", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews;
 
     // 연관관계 편의 메서드
     public void addAnimalPhoto(AnimalPhotos animalPhoto) {
