3cdf35173e79e723461b8b40a12fbefaccd9f5eb
= <=>
2024-05-25 22:05:38 +0900

    :art: CHORE. WebConfig 수정

--- src/main/java/com/hipet/global/config/WebConfig.java
+++ src/main/java/com/hipet/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://hipet.com", "https://hipet.com")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
