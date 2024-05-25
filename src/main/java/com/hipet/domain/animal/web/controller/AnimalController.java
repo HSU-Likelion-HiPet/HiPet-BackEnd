0e8e61a52ff0911d386b0bd5a684eeb7672fd1f1
= <=>
2024-05-25 21:02:36 +0900

    :sparkles: FEAT. 등록한 동물 상세보기 컨트롤러 메서드 작성

--- src/main/java/com/hipet/domain/animal/web/controller/AnimalController.java
+++ src/main/java/com/hipet/domain/animal/web/controller/AnimalController.java
@@ -2,15 +2,13 @@ package com.hipet.domain.animal.web.controller;
 
 import com.hipet.domain.animal.service.AnimalServiceImpl;
 import com.hipet.domain.animal.web.dto.AnimalRequestDto;
+import com.hipet.domain.animal.web.dto.GetOneAnimalResponseDto;
 import com.hipet.global.entity.response.CustomApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -22,8 +20,17 @@ public class AnimalController {
     // 동물 등록하기
     @PostMapping("/hipet")
     public ResponseEntity<CustomApiResponse<?>> addAnimals(@Valid @ModelAttribute AnimalRequestDto request){
+        log.info("request : {}", request);
+
         ResponseEntity<CustomApiResponse<?>> registerResponse = animalService.registerAnimals(request);
 
         return registerResponse;
+    }
+
+    @GetMapping("/{animalId}")
+    public ResponseEntity<CustomApiResponse<GetOneAnimalResponseDto.FinalResponseDto>> getAnimalPost(@Valid @PathVariable Long animalId){
+        ResponseEntity<CustomApiResponse<GetOneAnimalResponseDto.FinalResponseDto>> getAnimalInfo = animalService.getOneAnimal(animalId);
+
+        return getAnimalInfo;
     }
 }
