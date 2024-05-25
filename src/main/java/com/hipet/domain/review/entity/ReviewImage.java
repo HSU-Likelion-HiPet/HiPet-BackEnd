94c9bc6b099a75c1ee4c471b9eb68d08d24da022
= <=>
2024-05-25 21:11:00 +0900

    :sparkles: FEAT. 리뷰 엔티티 및 레포지토리 작성

---
+++ src/main/java/com/hipet/domain/review/entity/ReviewImage.java
@@ -0,0 +1,25 @@
+package com.hipet.domain.review.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReviewImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewImageId;
+
+    private String imageURL;
+
+    @ManyToOne
+    @JoinColumn(name = "reviewId")
+    private Review reviewId;
+}
