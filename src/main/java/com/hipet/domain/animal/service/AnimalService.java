78ff8dc867f628789cac4f6636555cdfc66c8bd3
= <=>
2024-05-27 03:02:35 +0900

    :sparkles: FEAT. 동물 상세보기 기능 구현

--- src/main/java/com/hipet/domain/animal/service/AnimalService.java
+++ src/main/java/com/hipet/domain/animal/service/AnimalService.java
@@ -1,11 +1,12 @@
 package com.hipet.domain.animal.service;
 
 import com.hipet.domain.animal.web.dto.AnimalRequestDto;
+import com.hipet.domain.animal.web.dto.GetOneAnimalRequestDto;
 import com.hipet.domain.animal.web.dto.GetOneAnimalResponseDto;
 import com.hipet.global.entity.response.CustomApiResponse;
 import org.springframework.http.ResponseEntity;
 
 public interface AnimalService {
     ResponseEntity<CustomApiResponse<?>> registerAnimals(AnimalRequestDto request);
-    ResponseEntity<CustomApiResponse<GetOneAnimalResponseDto.FinalResponseDto>> getOneAnimal(Long animalId);
+    CustomApiResponse<GetOneAnimalResponseDto.FinalResponseDto> getOneAnimal(GetOneAnimalRequestDto request);
 }
