925234ba4a812a395cb71b4355652b8e90677936
HyeJeong <rucy011121@gmail.com>
2024-05-29 17:28:01 +0900

    :sparkles: FEAT : 마이페이지 사진 수정 기능

--- src/main/java/com/hipet/domain/user/web/dto/UserPageUpdateDto.java
+++ src/main/java/com/hipet/domain/user/web/dto/UserPageUpdateDto.java
@@ -1,6 +1,7 @@
 package com.hipet.domain.user.web.dto;
 
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 public class UserPageUpdateDto {
     @Getter
@@ -9,11 +10,9 @@ public class UserPageUpdateDto {
     @AllArgsConstructor
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
     public static class Req {
-        private String userName; //사용자 이름
-        private String region; //사용자 주소
-        private String profileInfo; //사용자 소개
-        private String profilePhoto; //사용자 사진
-
-
+        private String userName; // 사용자 이름
+        private String region; // 사용자 주소
+        private String profileInfo; // 사용자 소개
+        private MultipartFile profilePhoto; // 사용자 사진
     }
 }
